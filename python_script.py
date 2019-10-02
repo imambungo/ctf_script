7398820488e7e6bdd760838598e915cb6d39b938
M. Imam Pratama <imambungo@gmail.com>
2019-10-02 22:44:36 +0700

    add function: bruteforce_hash256_menggunakan_file()

--- python_script.py
+++ python_script.py
@@ -12,11 +12,16 @@ def shell_cmd(COMMAND):
 
 # check if proposed password match the given hash
 def is_password_match_hash256(PASSWORD, HASH):
-    data = PASSWORD.encode('utf8')
-    sha256hash = hashlib.sha256(data).digest()
-    PASSWORD_HASH = binascii.hexlify(sha256hash).decode("utf-8")
+    PASSWORD_HASH = hash256(PASSWORD)
+
     result = HASH == PASSWORD_HASH
-    print(result)
     if result:
         exit(0)
     exit(1)
+
+
+def hash256(PASSWORD):
+    data = PASSWORD.encode('utf8')
+    sha256hash = hashlib.sha256(data).digest()
+    PASSWORD_HASH = binascii.hexlify(sha256hash).decode("utf-8")
+    return PASSWORD_HASH
